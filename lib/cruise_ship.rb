bf0d75934aa8abbb6ab0309603f46e1eac09a952
Damon Collins <damon.c.p.collins@gmail.com>
2018-10-29 00:19:08 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,5 +8,9 @@ suite_e: "Crumpet the Elf"
 
 def select_winner(passengers)
   # add the code snippet here!
-  
+  passengers.each do |k, v|
+    if k == suite_a && v[0] == "A"
+      return v 
+    end 
+  end 
 end
